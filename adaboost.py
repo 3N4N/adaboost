8a2434c78921c05154523e130d0de97fcb0e9798
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-12 05:46:52 +0600

    Finish adaboost

--- adaboost.py
+++ adaboost.py
@@ -6,9 +6,8 @@ import preproc
 from logistic_regression import LogisticRegression
 
 
-
 class Adaboost():
-    def __init__(self, X, y, n_clf=5):
+    def __init__(self, X, y, n_clf):
         self.n_clf = n_clf
         self.X = X
         self.y = y
@@ -22,8 +21,8 @@ class Adaboost():
         w = np.full(n_samples, (1 / n_samples))
         examples = np.c_[self.X, self.y]
 
-        # for k in range(self.n_clf):
-        while len(self.clfs) < 5:
+        for k in range(self.n_clf):
+        # while len(self.clfs) < self.n_clf:
             samples = examples[np.random.choice(range(n_samples), (n_samples,), replace=True, p=w),:]
             _X = samples[:, :-1]
             _y = samples[:, -1]
@@ -45,11 +44,12 @@ class Adaboost():
                     w[j] *= (error/(1-error))
             w /= w.sum()
             self.clfs.append(clf)
-            _z = np.log((1-error) / error)
+            _z = np.log(1-error) - np.log(error)
             self.z.append(_z)
 
         self.z = np.divide(self.z, np.sum(self.z))
         print(self.z)
+        self.n_clf = len(self.clfs)
 
     def predict(self, X, y):
         yhat = []
@@ -57,22 +57,33 @@ class Adaboost():
             yhat.append(self.z[k] * np.round(self.clfs[k].predict(X)))
         yhat = np.round(np.sum(np.array(yhat), axis=0))
 
-        score = 0
-        for i in range(X.shape[0]):
-            if yhat[i] == y[i]:
-                score += 1
-        score /= X.shape[0]
-        print("score:", score)
-
         return yhat
 
-
-def main():
-    X_train, X_test, y_train, y_test = preproc.process_telco()
-    adb = Adaboost(X_train, y_train)
+def run_adaboost():
+    # X_train, X_test, y_train, y_test = preproc.process_telco(0)
+    X_train, X_test, y_train, y_test = preproc.process_adult(None)
+    # X_train, X_test, y_train, y_test = preproc.process_credit(0)
+    adb = Adaboost(X_train, y_train, 5)
     adb.fit()
+
+    n_samples, n_features = np.shape(X_train)
     yhat = adb.predict(X_train, y_train)
+    score = 0
+    for i in range(n_samples):
+        if round(yhat[i]) == round(y_train[i]):
+            score += 1
+    score /= n_samples / 100
+    print("Train score:", score)
+
+    n_samples, n_features = np.shape(X_test)
+    yhat = adb.predict(X_test, y_test)
+    score = 0
+    for i in range(n_samples):
+        if round(yhat[i]) == round(y_test[i]):
+            score += 1
+    score /= n_samples / 100
+    print("Test score:", score)
 
 
 if __name__ == '__main__':
-    main()
+    run_adaboost()
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,7 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import math as m
+from sklearn.metrics import confusion_matrix as cm
 
 import preproc
 
@@ -30,11 +31,11 @@ class LogisticRegression():
         w = np.array([0.0 for i in range(n_features)])
         for itr in range(self.epoch):
             yhat = self.predict(self.X, w)
-            mismatch = 0
-            for i in range(n_samples):
-                if round(yhat[i]) != self.y[i]:
-                    mismatch += 1
-            mismatch /= n_samples
+            # mismatch = 0
+            # for i in range(n_samples):
+            #     if round(yhat[i]) != self.y[i]:
+            #         mismatch += 1
+            # mismatch /= n_samples
             # if (mismatch < 0.5):
             #     break
             dloss = ((self.y - yhat) * (1.0 - yhat*yhat) * self.X.T).mean(axis=1)
@@ -43,63 +44,37 @@ class LogisticRegression():
         self.w = w.flatten()
 
 
+def report(y, y_pred):
+    [[TN, FP], [FN, TP]] = cm(y, y_pred)
+    tpr = TP / (TP + FN)
+    tnr = TN / (TN + FP)
+    ppv = TP / (TP + FP)
+    fdr = FP / (FP + TP)
+    f1s = 2*TP / (2*TP + FP + FN)
+    print(TP, TN, FP, FN)
+    print("TPR:", tpr)
+    print("TNR:", tnr)
+    print("PPV:", ppv)
+    print("FDR:", fdr)
+    print("F1-Score:", f1s)
 
 
-def main():
-    # X_train, X_test, y_train, y_test = preproc.process_telco(10)
-    X_train, X_test, y_train, y_test = preproc.process_adult(10)
+def run_logistic_regression():
+    # X_train, X_test, y_train, y_test = preproc.process_telco(0)
+    X_train, X_test, y_train, y_test = preproc.process_adult(0)
+    # X_train, X_test, y_train, y_test = preproc.process_credit(0)
     lr = LogisticRegression(X_train, y_train)
     lr.sgd()
     print(lr.w)
 
-    n_samples, n_features = np.shape(X_train)
-    yhat = lr.predict(X_train)
-    score = 0
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-    for i in range(n_samples):
-        if round(yhat[i]) == round(y_train[i]):
-            score += 1
-            if round(yhat[i]) == 1:
-                tp += 1
-            else:
-                tn += 1
-        else:
-            if round(yhat[i]) == 1:
-                fp += 1
-            else:
-                fn += 1
-    score /= n_samples * 100
     print("Train set:")
-    print("Score:", score)
-    print("TP:", tp, "TN:", tn, "FP:", fp, "FN:", fn)
-
-    n_samples, n_features = np.shape(X_test)
-    yhat = lr.predict(X_test)
-    score = 0
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-    for i in range(n_samples):
-        if round(yhat[i]) == round(y_test[i]):
-            score += 1
-            if round(yhat[i]) == 1:
-                tp += 1
-            else:
-                tn += 1
-        else:
-            if round(yhat[i]) == 1:
-                fp += 1
-            else:
-                fn += 1
-    score /= n_samples * 100
+    yhat = lr.predict(X_train)
+    report(np.round(y_train), np.round(yhat))
     print("Test set:")
-    print("Score:", score)
-    print("TP:", tp, "TN:", tn, "FP:", fp, "FN:", fn)
+    yhat = lr.predict(X_test)
+    report(np.round(y_test), np.round(yhat))
+
 
 
 if __name__ == '__main__':
-    main()
+    run_logistic_regression()
