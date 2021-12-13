e3b7c172e948dbaaa8a7596a0cc50dabef47e525
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-13 20:33:49 +0600

    Remove matplotlib from dependencies

--- preproc.py
+++ preproc.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.feature_selection import mutual_info_classif
