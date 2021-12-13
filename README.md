9ae2ea86cd237791892bd00b873bac2b43c65210
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-13 21:35:07 +0600

    Fix: preprocessing adult dataset
    For some reason using `pandas.DataFrame.iloc` removes some data in the y
    column. That resulted in wrong prediction and wrong accuracy.
    
    So I changed splitting the data-frame into X and y parts from by using
    iloc to drop columns and data-frame column access

--- README.md
+++ README.md
@@ -4,37 +4,55 @@ In this assignment of CSE472 course we implement adaboost algorithm with
 logistic regression as weak learner to predict the binary labels of given
 datasets.
 
+# Datasets Used
+
+- https://www.kaggle.com/blastchar/telco-customer-churn
+- https://archive.ics.uci.edu/ml/datasets/adult
+- https://www.kaggle.com/mlg-ulb/creditcardfraud
+
+The first two datasets are saved in this repo. The third one is too large to
+push to GitHub, so you'll need to download it yourself.
+
+```bash
+wget -O creditcard.zip https://www.kaggle.com/mlg-ulb/creditcardfraud/download
+unzip creditcard.zip
+mv creditcard.csv ./data/
+```
+
 ## Report of Logistic Regression
 
 ### Telco dataset
 
 | Performance measure       | Training  | Test  |
 | :---                      | :---:     | :---: |
-| True positive rate        | 56%       | 52%   |
-| True negative rate        | 90%       | 90%   |
-| Positive predictive value | 67%       | 65%   |
-| False discovery rate      | 33%       | 36%   |
-| F1 score                  | 61%       | 57%   |
+| Accuracy                  | 80%       | 79%   |
+| True positive rate        | 53%       | 50%   |
+| True negative rate        | 89%       | 89%   |
+| Positive predictive value | 65%       | 63%   |
+| False discovery rate      | 34%       | 36%   |
+| F1 score                  | 58%       | 56%   |
 
 ### Adult dataset
 
 | Performance measure       | Training  | Test  |
 | :---                      | :---:     | :---: |
-| True positive rate        | 00%       | 00%   |
-| True negative rate        | 01%       | 01%   |
-| Positive predictive value | nan       | nan   |
-| False discovery rate      | nan       | nan   |
-| F1 score                  | 00%       | 00%   |
+| Accuracy                  | 84%       | 84%   |
+| True positive rate        | 58%       | 58%   |
+| True negative rate        | 92%       | 92%   |
+| Positive predictive value | 72%       | 71%   |
+| False discovery rate      | 27%       | 28%   |
+| F1 score                  | 64%       | 64%   |
 
 ### Credit Card Fraud dataset
 
 | Performance measure       | Training  | Test  |
 | :---                      | :---:     | :---: |
+| Accuracy                  | 42%       | 42%   |
 | True positive rate        | 94%       | 93%   |
 | True negative rate        | 41%       | 41%   |
 | Positive predictive value | 03%       | 03%   |
 | False discovery rate      | 96%       | 96%   |
-| F1 score                  |  7%       | 07%   |
+| F1 score                  | 07%       | 07%   |
 
 ## Report of Adaboost
 
@@ -50,15 +68,15 @@ datasets.
 
 | Number of boosting rounds | Training  | Test  |
 | :---:                     | :---:     | :---: |
-| 5                         | 99%       | 99%   |
-| 10                        | 99%       | 99%   |
-| 15                        | 99%       | 99%   |
+| 5                         | 84%       | 84%   |
+| 10                        | 84%       | 84%   |
+| 15                        | 84%       | 84%   |
 
 ### Credit Card Fraud dataset
 
 | Number of boosting rounds | Training  | Test  |
 | :---:                     | :---:     | :---: |
-| 5                         | 51%       | 51%   |
+| 5                         | 78%       | 78%   |
 | 10                        | 98%       | 98%   |
-| 15                        | 96%       | 96%   |
+| 15                        | 97%       | 97%   |
 
--- preproc.py
+++ preproc.py
@@ -20,33 +20,27 @@ def process_telco(percentile=None):
         df[c].replace(to_replace='No internet service', value='No', inplace=True)
 
 
-    # Encoding categorical data
-    le = LabelEncoder()
-
     labelcols = [
             'gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines',
             'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
             'TechSupport', 'StreamingTV', 'StreamingMovies',
-            'Contract', 'PaperlessBilling', 'PaymentMethod'
+            'Contract', 'PaperlessBilling', 'PaymentMethod', 'Churn'
     ]
+
+    onehotcols = [ 'InternetService', 'Contract', 'PaymentMethod' ]
 
     for c in labelcols:
         df[c] = LabelEncoder().fit_transform(df[c])
 
-    X = df.iloc[:, :-1].values
-    y = df.iloc[:, -1].values
+    for c in onehotcols:
+        df = pd.concat([df, pd.get_dummies(df[c], prefix=c)], axis=1)
+    df.drop(columns=onehotcols, axis=1, inplace=True)
 
+    imp = SimpleImputer(missing_values=np.NAN, strategy='mean')
+    df = pd.DataFrame(imp.fit_transform(df), columns=df.columns, index=df.index)
 
-
-    encol = [7,14,16]
-    ct = ColumnTransformer([("onehot", OneHotEncoder(), encol)], remainder='passthrough')
-    X = ct.fit_transform(X)
-
-    y = LabelEncoder().fit_transform(y)
-
-
-    imp = SimpleImputer(missing_values=np.NAN, strategy= 'mean')
-    X[:,25] = imp.fit_transform(X[:,25].reshape(-1,1))[:,0]
+    X = df.drop(columns=['Churn']).to_numpy()
+    y = df['Churn'].to_numpy()
 
     if percentile is not None and percentile != 0:
         X = SelectPercentile(mutual_info_classif, percentile=percentile).fit_transform(X, y)
@@ -60,9 +54,6 @@ def process_telco(percentile=None):
     X_train = sc_X.fit_transform(X_train)
     X_test = sc_X.transform(X_test)
 
-    # X_train = X_train[:,:10]
-    # X_test = X_test[:,:10]
-
     X_train = np.c_[X_train, np.ones(X_train.shape[0])]
     X_test  = np.c_[X_test,  np.ones(X_test.shape[0])]
 
@@ -74,16 +65,16 @@ def process_adult(percentile=None):
             'age', 'workclass', 'fnlwgt', 'education',
             'education-num', 'marital-status', 'occupation',
             'relationship', 'race', 'sex', 'capital-gain',
-            'capital-loss', 'hours-per-week', 'native-country', 'morethan50k'
+            'capital-loss', 'hours-per-week', 'native-country', 'salary'
     ]
     numeric_cols = [
             'age', 'fnlwgt', 'education-num', 'capital-gain',
             'capital-loss', 'hours-per-week'
     ]
-    yes_no_colms = ['sex', 'morethan50k']
+    yes_no_colms = ['sex', 'salary']
     yes_no_map = [{'Male': 1, 'Female': 0}, {'>50K': 1, '<=50K': 0}]
-    cat_colms = [cat for cat in columns[:-1] if cat not in numeric_cols]
-    cat_colms = [cat for cat in cat_colms if cat not in yes_no_colms]
+    labelcols = [cat for cat in columns[:-1] if cat not in numeric_cols]
+    labelcols = [cat for cat in labelcols if cat not in yes_no_colms]
 
     df = pd.read_csv('data/adult/adult.data', header=None, names=columns)
     df_test = pd.read_csv('data/adult/adult.test', header=None, names=columns).drop(0)
@@ -100,9 +91,9 @@ def process_adult(percentile=None):
     imp = SimpleImputer(missing_values=np.NAN, strategy='most_frequent')
     df = pd.DataFrame(imp.fit_transform(df), columns=df.columns, index=df.index)
 
-    for c in cat_colms:
+    for c in labelcols:
         df = pd.concat([df, pd.get_dummies(df[c], prefix=c)], axis=1)
-    df.drop(columns=cat_colms, axis=1, inplace=True)
+    df.drop(columns=labelcols, axis=1, inplace=True)
 
     for i in range(len(yes_no_colms)):
         df[yes_no_colms[i]].replace(yes_no_map[i], inplace=True)
@@ -110,10 +101,8 @@ def process_adult(percentile=None):
     for c in numeric_cols:
         df[c] = pd.to_numeric(df[c])
 
-    # df.to_csv("adult.csv")
-
-    X = df.iloc[:, :-1].values
-    y = df.iloc[:, -1].values
+    X = df.drop(columns=['salary']).to_numpy()
+    y = df['salary'].to_numpy()
 
     if percentile is not None and percentile != 0:
         X = SelectPercentile(mutual_info_classif, percentile=percentile).fit_transform(X, y)
@@ -151,8 +140,8 @@ def process_credit(percentile=None, n_neg_samples=20000):
         df = df.sample(frac=1, random_state=0).reset_index(drop=True)
         df.drop(columns=['index'], axis=0, inplace=True)
 
-    X = df.iloc[:, :-1].values
-    y = df.iloc[:, -1].values
+    X = df.drop(columns=['Class']).to_numpy()
+    y = df['Class'].to_numpy()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -161,7 +150,7 @@ def process_credit(percentile=None, n_neg_samples=20000):
 
 if __name__ == '__main__':
     print("Running preprocessing . . .")
-    # X_train, X_test, y_train, y_test = process_telco()
-    # X_train, X_test, y_train, y_test = process_adult()
+    X_train, X_test, y_train, y_test = process_telco()
+    X_train, X_test, y_train, y_test = process_adult()
     X_train, X_test, y_train, y_test = process_credit()
     print("Finished preprocessing.")
